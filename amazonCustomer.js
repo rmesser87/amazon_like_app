d56df64ad798bdc2f7857d4b70dd66a31ec8b584
Ross Messer <rmesser@perennial.local>
2018-04-17 22:30:55 -0500

    more js changes

--- amazonCustomer.js
+++ amazonCustomer.js
@@ -18,33 +18,48 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     readProducts();
-    initialAction();
 });
 
 function readProducts() {
     connection.query("select * from products", function (err, res) {
         if (err) throw err;
-        console.log(Table.print(res));;
+        console.log(Table.print(res));
+        initialAction();
         //   connection.end();
     });
 };
 
 function initialAction() {
-    inquirer
-        .prompt({
-            name: "chooseProduct",
-            type: "input",
-            message: "What is the ID (user_id) of the product you would like to buy?"
-        }, {
-            name: "quanity",
-            type: "input",
-            message: "How many units would you like to buy?"
-        })
-        .then(function (answer) {
-            if (answer.postOrBid.toUpperCase() === "POST") {
-                postAuction();
-            } else {
-                bidAuction();
+    var questions = [{
+        name: "chooseProduct",
+        type: "input",
+        message: "What is the ID (user_id) of the product you would like to buy?",
+        validate: function(value) {
+            if (isNaN(value) === false && value % 1 == 0) {
+              return true;
             }
+            return false;
+            console.log("Please enter a valid ID and quantity")
+            throw err;
+            
+          }
+    }, {
+        name: "quanity",
+        type: "input",
+        message: "How many units would you like to buy?",
+        validate: function(value) {
+            if (isNaN(value) === false && value % 1 == 0) {
+              return true;
+            }
+            return false;
+            console.log("Please enter a valid ID and quantity")
+            throw err;
+          }
+    }];
+    inquirer
+        .prompt(questions)
+        .then(function (answer) {
+            // (Number.isInteger(answer.chooseProduct.value)
+            console.log("------Searching Inventory------");
         });
 }
